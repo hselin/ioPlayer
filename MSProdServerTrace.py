2c59abba4ee9fefd58c5a68a3fbaa4625c8d3f00
Albert Chen <albert@rstor.io>
2017-08-09 12:24:23 -0700

    0

---
+++ MSProdServerTrace.py
@@ -0,0 +1,12 @@
+
+import pandas as pd
+import numpy as np
+
+class Trace(object):
+    def __init__(self):
+        self.name = "moo"
+
+
+class MSProdServerTrace(Trace):
+    def __init__(self):
+        self.name = "MSProdServerTrace"
